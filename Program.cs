32fa1ef0cda30d09d30f7c314ffa30f551610135
Tim Mereien <t.mereien@omicron-laser.de>
2022-05-09 14:35:15 +0200

    added equation solver endpoint

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/calc/add", (int a, int b) => { return a + b; });
+app.MapGet("/calc/add", (int a, int b) => {     return a + b; });
 app.MapGet("/calc/subtract", (int a, int b) => { return a - b; });
 app.MapGet("/calc/multiply", (int a, int b) => { return a * b; });
 
+app.MapGet("/calculator/{equation}", (String equation) => {
+    DataTable dt = new DataTable();
+    var v = dt.Compute(equation, "");
+    return v; 
+});
 app.Run();
